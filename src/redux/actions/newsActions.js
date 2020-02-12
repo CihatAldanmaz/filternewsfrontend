5cfb0956c37edb6f7d6a00c918c4b3fd5490cb0f
CihatAld <cihataldanmaz@gmail.com>
2020-02-12 06:01:32 -0500

    95 percent done

--- src/redux/actions/newsActions.js
+++ src/redux/actions/newsActions.js
@@ -13,9 +13,9 @@ export function getNewsSuccess(news){
 
 export function getNews(categoryName){
     return function(dispatch){
-        let url = `https://newsapi.org/v2/top-headlines?sources=the-new-york-times,cnn,the-wall-street-journal,abc-news,fox-news,cbs-news,the-washington-post,&apiKey=!!apikey!!&pageSize=100`;
+        let url = `https://newsapi.org/v2/top-headlines?sources=the-new-york-times,cnn,the-wall-street-journal,abc-news,fox-news,cbs-news,the-washington-post,&apiKey=72eb57002bca4d42a85404eef1e97d2a&pageSize=100`;
         if(categoryName){
-            url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoryName}&apiKey=!!apikey!!&pageSize=100`
+            url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoryName}&apiKey=72eb57002bca4d42a85404eef1e97d2a&pageSize=100`
         }
         
         return fetch(url)
@@ -28,7 +28,7 @@ export function getNews(categoryName){
 
 export function filterNews(source){
     return function(dispatch){
-        let url = `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=!!apikey!!&pageSize=100`;
+        let url = `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=72eb57002bca4d42a85404eef1e97d2a&pageSize=100`;
 
         return fetch(url)
         .then(resp => resp.json())
@@ -38,7 +38,7 @@ export function filterNews(source){
 
 export function searchNews(word){
     return function(dispatch){
-       let url = `https://newsapi.org/v2/top-headlines?country=us&q=${word}&apiKey=!!apikey!!`
+       let url = `https://newsapi.org/v2/top-headlines?country=us&q=${word}&apiKey=72eb57002bca4d42a85404eef1e97d2a`
     
 
     return fetch(url)
