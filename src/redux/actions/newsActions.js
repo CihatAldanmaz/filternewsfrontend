2734c14edb66873ff2daebffefb5d7ab97ecdd31
CihatAldanmaz <46700874+CihatAldanmaz@users.noreply.github.com>
2020-02-13 00:51:07 -0500

    Update newsActions.js

--- src/redux/actions/newsActions.js
+++ src/redux/actions/newsActions.js
@@ -13,7 +13,7 @@ export function getNewsSuccess(news){
 
 export function getNews(categoryName){
     return function(dispatch){
-        let url = `https://newsapi.org/v2/top-headlines?sources=the-new-york-times,cnn,the-wall-street-journal,abc-news,fox-news,cbs-news,the-washington-post,&apiKey=72eb57002bca4d42a85404eef1e97d2a&pageSize=100`;
+        let url = `https://newsapi.org/v2/top-headlines?sources=the-new-york-times,cnn,the-wall-street-journal,abc-news,fox-news,cbs-news,&apiKey=72eb57002bca4d42a85404eef1e97d2a&pageSize=100`;
         if(categoryName){
             url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoryName}&apiKey=72eb57002bca4d42a85404eef1e97d2a&pageSize=100`
         }
